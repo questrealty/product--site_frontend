cfbf018f80c3f8f6f96815de63df67edeec3534c
motuncoded <motunrayoadeneye@example.com>
2022-07-06 00:11:29 +0100

    setting up the environment

---
+++ quest-realty/src/pages/Home.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+<header>
+  <img src="../assets/logo.png" alt="quest-realty-logo"/>
+  <ul>
+    <li>Home</li>
+     <li>About</li>
+      <li>For Sale</li>
+            <li>For Rent</li>
+
+    
+  </ul>
+  <div>
+    <button>Login</button>
+        <button>Register</button>
+
+  </div>
+</header> 
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
