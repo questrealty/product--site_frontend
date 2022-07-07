dedb63706df37bff488035c72e51b4808008089a
PraiseDelia <elemdelia@gmail>
2022-07-07 11:26:22 +0100

    Feat: start creating agents and services section
    I started building this agents and services section, however, it is not yet organized

--- quest-realty/src/App.vue
+++ quest-realty/src/App.vue
@@ -1,16 +1,20 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Agents from "./components/Agents.vue";
 </script>
 
 <template>
-<router-view/>
+  <div>
+    <Agents></Agents>
+    <router-view />
+  </div>
 </template>
 
 <style>
 #app {
-font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ quest-realty/src/components/Agents.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container">
+    <h3>Agents and Partners</h3>
+    <div class="image__container">
+      <div class="agents-1">
+        <img src="../assets/Frame-299-1.png" alt="" class="image1" />
+        <img src="../assets/Frame-300-1.png" alt="" class="image2" />
+      </div>
+
+      <div class="agents-2">
+        <img src="../assets/Frame-302.png" alt="" class="image3" />
+        <img src="../assets/Frame-301-1.png" alt="" class="image4" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
+
+<style scoped>
+@import url(https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;800;900&display=swap);
+
+.container {
+  width: 100%;
+  font-family: "Poppins", sans-serif;
+  background-color: #ede5df;
+  padding: 16px 20px;
+}
+
+h3 {
+  font-size: 40px;
+  font-weight: 700;
+  color: #6d553e;
+  text-align: center;
+  margin-bottom: 64px;
+}
+
+.image__container {
+  display: flex;
+  flex-direction: column;
+  /* justify-content: space-between; */
+}
+.agents-1,
+.agents-2 {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 24px;
+}
+
+img {
+  max-width: 150px;
+}
+
+@media (min-width: 460px) {
+  img {
+    max-width: 250px;
+  }
+}
+
+@media (min-width: 640px) {
+}
+
+@media (min-width: 786px) {
+  .container {
+    padding: 74px 80px;
+  }
+  img {
+    max-width: 200px;
+  }
+  .image__container {
+    flex-direction: row;
+  }
+}
+</style>
