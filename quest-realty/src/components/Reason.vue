9bdc5f147f89f50b8c2792012b4ce08b593fad1f
motuncoded <motunrayoadeneye@example.com>
2022-07-13 13:30:07 +0100

    Why people use quest realty

---
+++ quest-realty/src/components/Reason.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="reason-container">
+<div class="reason-main">
+    <div class="left-main">
+    <img class="frame-two" src="../assets/Frame 290.png" alt="quest-realty"/>
+
+</div>
+<div class="right-main">
+    <h2>Why people Choose 
+Quest Realty</h2>
+<p></p>
+
+</div>
+</div>
+</div>
+
+</template>
+
+<style scoped>
+.reason-container{
+    width: 100%;
+    height:550px;
+    background-color: #EDE5DF;
+    display:flex;
+  flex-direction:column;
+}
+.reason-main{
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    padding:1rem;
+    
+}
+.left-main{
+}
+.right-main{
+    top: 156px;
+  left: 675px;
+  width: 700px;
+  height: 350px;
+}
+.frame-two{
+    width:500px;
+}
+</style>
