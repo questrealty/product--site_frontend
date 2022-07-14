a5ea1ac0cccb9b5a73fb75d74c4f9276451cd204
PraiseDelia <elemdelia@gmail>
2022-07-14 10:04:52 +0100

    refactor: refactoring code

---
+++ src/pages/Home.vue
@@ -0,0 +1,19 @@
+<script setup>
+import FirstSection from "../components/home/first-section.vue";
+defineProps({
+  msg: String,
+});
+</script>
+
+<template>
+  <div class="home">
+    <FirstSection />
+    <Body />
+  </div>
+</template>
+
+<style scoped>
+.home {
+  width: 100%;
+}
+</style>
