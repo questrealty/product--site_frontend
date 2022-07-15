cd0df00ba824cf61201b1b63ff7ff66e1c381b63
PraiseDelia <elemdelia@gmail>
2022-07-15 12:29:56 +0100

    ...

---
+++ src/components/home/TopExecutives.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container flex">
+    <h1>Meet our Top Executives</h1>
+    <div class="inner-container flex">
+      <img src="../../assets/executive-1" alt="First Executive" />
+      <img src="../../assets/executive-2" alt="Second Executive" />
+      <img src="../../assets/executive-3" alt="Third Executive" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+/* h1 {
+} */
+
+.inner-container {
+  width: 80%;
+  flex-wrap: wrap;
+}
+</style>
